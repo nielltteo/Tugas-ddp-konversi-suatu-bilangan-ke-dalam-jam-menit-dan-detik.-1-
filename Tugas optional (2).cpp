d6b4112dac0d0cc8f29395c00f610ce49dc76649
Novalino Dheza Yonansyah <2500018020@webmail.uad.ac.id>
2025-11-03 10:40:29 +0700

    Refactor code formatting in Tugas optional (2).cpp

--- Tugas optional (2).cpp
+++ Tugas optional (2).cpp
@@ -9,29 +9,16 @@ int main() {
     cout << ">>";
     cin >> total_detik;
     
-    // Hitung hari
     hari = total_detik / 86400;
-    
-    // Hitung sisa detik setelah hari
-    int sisa_hari = total_detik % 86400;
-    
-    // Hitung jam dari sisa hari
-    jam = sisa_hari / 3600;
-    
-    // Hitung sisa detik setelah jam
+    int sisa_hari = total_detik % 86400;   
+    jam = sisa_hari / 3600;       
     int sisa_jam = sisa_hari % 3600;
-    
-    // Hitung menit dari sisa jam
-    menit = sisa_jam / 60;
-    
-    // Hitung detik dari sisa jam
+
+    menit = sisa_jam / 60;   
     detik = sisa_jam % 60;
-    
-    // Output hasil konversi
     cout << "Hasil Konversi:" << endl;
     cout << total_detik << " detik = " << hari << " Hari, " << jam << " Jam, " << menit << " Menit, " << detik << " Detik." << endl;
     
     return 0;
 }
-
 

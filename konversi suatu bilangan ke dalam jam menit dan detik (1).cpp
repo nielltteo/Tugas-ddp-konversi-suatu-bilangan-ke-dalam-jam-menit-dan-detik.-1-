4872b8cf010dd8b659b6ab4428baa97ac6d524fd
Novalino Dheza Yonansyah <2500018020@webmail.uad.ac.id>
2025-11-03 10:43:02 +0700

    Update konversi suatu bilangan ke dalam jam menit dan detik (1).cpp

--- konversi suatu bilangan ke dalam jam menit dan detik (1).cpp
+++ konversi suatu bilangan ke dalam jam menit dan detik (1).cpp
@@ -9,22 +9,14 @@ int main() {
     cout << ">>";
     cin >> total_detik;
     
-    // Hitung jam
     jam = total_detik / 3600;
-    
-    // Hitung sisa detik setelah jam
     int sisa = total_detik % 3600;
-    
-    // Hitung menit dari sisa
     menit = sisa / 60;
-    
-    // Hitung detik dari sisa
     detik = sisa % 60;
-    
-    // Output hasil konversi
     cout << "Hasil Konversi:" << endl;
     cout << total_detik << " detik = " << jam << " Jam, " << menit << " Menit, " << detik << " Detik." << endl;
     
     return 0;
 }
+
 
